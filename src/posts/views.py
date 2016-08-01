3a130af46cd91b875c4d59c2d084b3c9dbd84d5f
phani05353 <phani05353@gmail.com>
2016-08-01 17:35:38 +0000

    secpnd

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,16 +1,50 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .forms import PostForm
+from .models import Post
 
 def post_create(request):
-  return HttpResponse("<h1> Create </h1>")
+    form =PostForm(request.POST or None)
+    if  form.is_valid():
+        instance =form.save(commit=False)
+        print form.cleaned_data.get("title")
+        instance.save()
 
-def post_detail(request):  #retreive
-  return HttpResponse("<h1> Post </h1>")
+    #if request.method == "POST":
+      #print "title" + request.POST.get("content")
+      #print request.POST.get("title")
+      #Post.objects.create(title=title)
+    context = {
+      "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+def post_detail(request, id):  #retreive
+  instance = get_object_or_404(Post, id =id)
+  context = {
+    "title" : instance.title,
+    "instance": instance,
+  }
+  return render(request, "post_detail.html" , context)
+  #return HttpResponse("<h1> Post </h1>")
 
 def post_list(request): #list items
-  return HttpResponse("<h1> List them all </h1>")
+  queryset = Post.objects.all()
+  context = {
+    "object_list": queryset,
+    "title" :"List"
+  }
+  #if request.user.is_authenticated():
+    #context = {
+    #"title" :"My User List"
+  #}
+  #else:
+  #  context = {
+   # "title" :"List"
+  #}
+  return render(request, "index.html" , context)
+  #return HttpResponse("<h1> List them all </h1>")
 
 def post_update(request):
   return HttpResponse("<h1> Update </h1>")
